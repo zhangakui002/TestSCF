b00ba66112855f85fd56b714cc7e930b11608ef7
zhangakui002 <527982706@qq.com>
2021-09-07 15:21:09 +0800

    no message

---
+++ ClassLibrary1/SCFHostService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace ClassLibrary1
+{
+    public class SCFHostService:IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
